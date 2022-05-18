75d88c56d813e76beee70e51355ac0a3cc57465f
jbrizzio <jbrizzio@interbanking.com.ar>
2022-05-18 18:02:28 -0300

    Componentes II - contador con boton

---
+++ src/components/ItemCount.js
@@ -0,0 +1,36 @@
+import React from "react"
+import '../App.css';
+import { useState, useEffect } from 'react';
+
+const ItemCount = ({ stock, initial , onAdd }) => {
+    const [contador, setContador] = useState();
+    
+    const restar = () => setContador(contador > 1 ? contador - 1 : contador);
+    const sumar = () => setContador(contador < stock ? contador + 1 : stock);
+    const validar = () => {
+        if(contador > 0) {
+            onAdd(contador)
+        }
+    }
+    useEffect(
+        () => {
+            setContador( (initial !== undefined && initial < stock) ? Number(initial) : stock);
+        }, [initial, stock]
+    );
+
+    return(
+        
+        <div className="containerCount">
+            <p className="titulo">Producto</p>
+            <div className="contador">
+                <button onClick={ restar } name='menos'>-</button>
+                <span>{contador}</span>
+                <button onClick={ sumar } name="mas">+</button>
+            </div>
+            <div className="botonCarrito">
+            <button onClick={ validar }>Agregar al carrito</button>
+            </div>
+        </div>
+    )
+}
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,17 @@
 import React from "react"
 import '../App.css';
+import ItemCount from "./ItemCount"
 
 export const ItemListContainer = ({greeting}) => {
+    const nombre = ( valor ) => {
+        return (
+            console.log(valor)
+        )
+    }
     return(
         <div className="containerList">
             {greeting}
+            <ItemCount stock='10' initial='1' onAdd={ nombre } />
         </div>
     )
 }
